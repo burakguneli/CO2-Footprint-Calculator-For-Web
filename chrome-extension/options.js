a09d179b24200b150b67f6b51b26c374c320dbe1
Burak Güneli <burak.guneli@gmail.com>
2020-10-05 20:34:57 +0300

    feat: All time CO2 emission calculation

--- chrome-extension/options.js
+++ chrome-extension/options.js
@@ -1,5 +1,19 @@
 let page = document.getElementById("buttonDiv");
+let allTimeCO2Amount = 0;
 
-chrome.storage.local.get("totalCO2Emission", function (data) {
-	allTimeCO2.textContent = data.totalCO2Emission || "No data found!";
+chrome.storage.local.get(null, function (data) {
+	allTimeCO2Amount = Object.values(data).reduce(
+		(accumulator, currentData) => {
+			let newTotalEmission = accumulator;
+
+			if (typeof currentData === "object" && currentData.tabCO2Emission) {
+				newTotalEmission = accumulator + currentData.tabCO2Emission;
+			}
+
+			return newTotalEmission;
+		},
+		0
+	);
+
+	allTimeCO2.textContent = allTimeCO2Amount || "No data found!";
 });
