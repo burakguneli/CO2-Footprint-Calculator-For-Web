818563bd8e50d52d96d9150cbda4d2d25c4dcadb
Burak Güneli <burak.guneli@gmail.com>
2021-01-28 23:14:59 +0300

    fix(detail): Update detail page

--- chrome-extension/options.js
+++ chrome-extension/options.js
@@ -1,21 +1,21 @@
 let page = document.getElementById("buttonDiv");
 let allTimeCO2Amount = 0;
 
-chrome.storage.local.get(null, function (data) {
-	allTimeCO2Amount = Object.values(data).reduce(
-		(accumulator, currentData) => {
-			let newTotalEmission = accumulator;
+// chrome.storage.local.get(null, function (data) {
+// 	allTimeCO2Amount = Object.values(data).reduce(
+// 		(accumulator, currentData) => {
+// 			let newTotalEmission = accumulator;
 
-			if (typeof currentData === "object" && currentData.tabCO2Emission) {
-				newTotalEmission = accumulator + currentData.tabCO2Emission;
-			}
+// 			if (typeof currentData === "object" && currentData.tabCO2Emission) {
+// 				newTotalEmission = accumulator + currentData.tabCO2Emission;
+// 			}
 
-			return newTotalEmission;
-		},
-		0
-	);
+// 			return newTotalEmission;
+// 		},
+// 		0
+// 	);
 
-	document.getElementById("allTimeEmission").textContent = data.allTimeEmission;
+// 	document.getElementById("allTimeEmission").textContent = data.allTimeEmission;
 
-	allTimeCO2.textContent = allTimeCO2Amount || "No data found!";
-});
+// 	allTimeCO2.textContent = allTimeCO2Amount || "No data found!";
+// });
